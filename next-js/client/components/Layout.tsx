a8d2975541e49ff414b9680ad5d7619a63911602
tonyw0527 <tonyw0527@gmail.com>
2021-01-23 22:12:11 +0900

    Updated _document

--- next-js/client/components/Layout.tsx
+++ next-js/client/components/Layout.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from "react";
-import Head from "next/head";
 import Header from "./Header";
 
 const layoutStyle = {
@@ -15,17 +14,6 @@ type LayoutProps = {
 
 const Layout = ({ children, onToggleTheme }: LayoutProps) => (
   <div style={layoutStyle}>
-    <Head>
-      <meta charSet="utf-8" />
-      <link rel="icon" href="/favicon.ico" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta
-        name="description"
-        content="Tony West's Next.js App Boilder Plate"
-      />
-      <title>Next App</title>
-    </Head>
-
     <Header onToggleTheme={onToggleTheme} />
     {children}
   </div>
--- next-js/client/pages/_document.tsx
+++ next-js/client/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
@@ -26,5 +26,26 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <meta name="description" content="성경 읽기 사이트입니다." />
+          <title>성경 앱</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
